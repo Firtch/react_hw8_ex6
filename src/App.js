01dabd5469e5f08e946f069da1d2078c709773ed
Firtch <firtch@gmail.com>
2021-07-03 06:19:21 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Programmer,
+  Frontend,
+  Backend,
+  Fullstack,
+} from "./components/inheritance";
+import { createProgrammer, createFrontend, createBackend, createFullstack } from "./components/composition";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    console.log("Inheritance");
+
+    const programmer = new Programmer("Programmer");
+    programmer.code();
+
+    const frontend = new Frontend("Frontend");
+    frontend.code();
+    frontend.angular();
+
+    const backend = new Backend("Backend");
+    backend.code();
+    backend.nodejs();
+
+    const fullstack = new Fullstack("Fullstack");
+    fullstack.code();
+    fullstack.angular();
+    fullstack.nodejs();
+
+    console.log("Composition");
+
+    const programmerComp = createProgrammer("Programmer");
+    programmerComp.code();
+
+    const frontendComp = createFrontend("Frontend");
+    frontendComp.code();
+    frontendComp.angular();
+    frontendComp.react();
+    frontendComp.vue();
+
+    const backendComp = createBackend("Backend");
+    backendComp.code();
+    backendComp.nodejs();
+
+    const fullstackComp = createFullstack("Fullstack");
+    fullstackComp.code();
+    fullstackComp.angular();
+    fullstackComp.nodejs();
+
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
